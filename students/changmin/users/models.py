4c8cef742afc538548a0c4bf46f69d11f86920f9
Ted0527 <85148597+Ted0527@users.noreply.github.com>
2021-11-18 17:22:22 +0900

    Update models.py

--- students/changmin/users/models.py
+++ students/changmin/users/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class User(models.Model):
-    name    = models.CharField(max_length=45)
-    email   = models.CharField(max_length=45)
-    pwd     = models.CharField(max_length=20)
-    phone   = models.CharField(max_length=20)
+    name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45, unique=True)
+    password = models.CharField(max_length=200)
+    user_phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     class Meta:
         db_table = 'users'
