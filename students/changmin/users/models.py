1a530cf29baa37adc254fdad49cd6d345ff68c5d
Ted0527 <85148597+Ted0527@users.noreply.github.com>
2021-11-18 16:18:13 +0900

    Update models.py

--- students/changmin/users/models.py
+++ students/changmin/users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    user_name = models.CharField(max_length=45, verbose_name='이름')
-    user_email = models.CharField(max_length=45, verbose_name='이메일')
-    user_pwd = models.CharField(max_length=20, verbose_name='비밀번호')
-    user_phone = models.CharField(max_length=20, verbose_name='전화번호')
+    name    = models.CharField(max_length=45)
+    email   = models.CharField(max_length=45)
+    pwd     = models.CharField(max_length=20)
+    phone   = models.CharField(max_length=20)
     
     class Meta:
         db_table = 'users'
