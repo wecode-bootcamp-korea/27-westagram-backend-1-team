619418de1ec6ff8447d29eb2a51289605e128569
Jaeyong Park <jaejayjaejaypark@gmail.com>
2021-11-18 18:40:53 +0900

    모델링_수정

--- students/jaeyong/users/models.py
+++ students/jaeyong/users/models.py
@@ -7,5 +7,8 @@ class User(models.Model):
     phone = models.CharField(max_length = 15)
     introduce = models.CharField(max_length=150)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     class Meta:
         db_table = 'users'
