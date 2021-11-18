5ed8021e61c37af298b5f6108a3e060364168b59
EslerKang <eslerkang@gmail.com>
2021-11-18 16:55:08 +0900

    WIT

--- students/taejun/users/views.py
+++ students/taejun/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+import json, re
+
+from django.views import View
+from django.http  import JsonResponse
 
 # Create your views here.
+
+class UsersView(View):
+    def post(self, req):
+        data = json.loads(req.body)
+        try:
+            name     = data['name']
+            email    = data['email']
+            password = data['password']
+            contact  = data['contact']
+            if 'mbti' in data:
+                mbti = data['mbti']
+            if 'gender' in data:
+                gender = data['gender']
+                if gender not in ['F', 'M', 'U']:
+                    raise ValueError
+
+        except:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+
+        return JsonResponse({'RESULT': 'SUCCESS'}, status=201)
