8c6e032e235c996b20a568e4b18e0220c2874eec
Ted0527 <frosthowling@gmail.com>
2021-11-19 20:12:46 +0900

    [제갈창민] Mission 3: 회원가입 기능 구현

--- students/changmin/users/views.py
+++ students/changmin/users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import json, re
 
-# Create your views here.
+from django.views     import View
+from django.http      import JsonResponse
+from users.models     import User
+
+class UserView(View):
+    def post(self, request):
+        data           = json.loads(request.body)
+        regex_email    = '[a-zA-Z0-9]+\.?\w*@\w+[.]?\w*[.]+\w{2,3}'
+        regex_password = '(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$^&*()+|=])[A-Za-z\d~!@#$%^&*()+|=]{8,}'
+
+        try:
+            if data['email'] == '' or data['password'] == '':
+                return JsonResponse({'message':'Expected any words in Box'}, status = 400)
+
+            elif data['email'] in [i.email for i in User.objects.all()]:
+                raise JsonResponse({'message' : 'Duplicated Email address'}, status=400)
+
+            elif re.match(regex_email, data['email']) is None or re.match(regex_password, data['password']) is None:
+                raise ValueError
+
+        except ValueError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+            
+        else:
+            User.objects.create(
+                name        = data['name'],
+                email       = data['email'],
+                password    = data['password'],
+                phone     = data['phone'],
+            )
+            return JsonResponse({'message':'SUCESS'},status=201)
