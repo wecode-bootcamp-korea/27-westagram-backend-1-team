38183d25f3722208ec9f894e6827a07dedf3c857
이태연 <itaeyeon@itaeyeon-ui-MacBookAir.local>
2021-11-19 15:10:12 +0900

    real real  final  models.py

--- students/taeyeon/users/models.py
+++ students/taeyeon/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model) :
     name       = models.CharField(max_length=45)
     email      = models.CharField(max_length=45,unique=True)
     password   = models.CharField(max_length=200)
-    phone      = models.CharField(max_length=11)
+    phone      = models.CharField(max_length=20)
     mbti       = models.CharField(max_length=4, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
