92e4717a5bebe2388e0f845bf63d46d6f77de252
yajkrap <93360693+yajkrap@users.noreply.github.com>
2021-11-19 15:11:26 +0900

    Update models.py

--- students/jaeyong/users/models.py
+++ students/jaeyong/users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class User(models.Model):
-    name     = models.CharField(max_length=30)
-    email    = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=128)
-    phone    = models.CharField(max_length=15)
-    bio      = models.CharField(max_length=150)
+    name       = models.CharField(max_length=30)
+    email      = models.CharField(max_length=255, unique=True)
+    password   = models.CharField(max_length=128)
+    phone      = models.CharField(max_length=35)
+    bioprofile = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
