4f5d31bb3c9edd135bb6e156de4a4b97f4484170
이태연 <itaeyeon@itaeyeon-ui-MacBookAir.local>
2021-11-19 11:02:26 +0900

    real final  models.py

--- students/taeyeon/users/models.py
+++ students/taeyeon/users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class User(models.Model) :
-    name = models.CharField(max_length=45)
-    email = models.CharField(max_length=45,unique=True)
-    password = models.CharField(max_length=200)
-    phone = models.CharField(max_length=11)
-    mbti = models.CharField(max_length=4, null=True)
+    name       = models.CharField(max_length=45)
+    email      = models.CharField(max_length=45,unique=True)
+    password   = models.CharField(max_length=200)
+    phone      = models.CharField(max_length=11)
+    mbti       = models.CharField(max_length=4, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
