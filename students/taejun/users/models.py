9ed7ff5e54512c491d89d0859029a1e71dc3441f
EslerKang <eslerkang@gmail.com>
2021-11-19 14:12:51 +0900

    taejun / modified SignupView

--- students/taejun/users/models.py
+++ students/taejun/users/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     password   = models.CharField(max_length=200)
     contact    = models.CharField(max_length=11, unique=True)
     mbti       = models.CharField(max_length=4, null=True, blank=True)
-    gender     = models.CharField(max_length=1, default='U')
+    gender     = models.CharField(max_length=9, default='Undefined')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- students/taejun/users/views.py
+++ students/taejun/users/views.py
@@ -6,48 +6,55 @@ from django.http  import JsonResponse
 from users.models import User
 
 class SignupView(View):
-    def post(self, req):
-        data = json.loads(req.body)
+
+    def post(self, request):
+
+        data = json.loads(request.body)
+
         try:
             name     = data['name']
             email    = data['email']
             password = data['password']
             contact  = data['contact']
+            mbti     = data['mbti']   if 'mbti'   in data else None
+            gender   = data['gender'] if 'gender' in data else 'Undefined'
 
-            email_regex = '^[a-zA-Z0-9]([-_.]?[a-zA-Z0-9])*@[a-zA-Z0-9]([-_.]?[a-zA-Z0-9])*\.[a-zA-Z]{2,3}$'
+            email_regex    = '^[a-zA-Z0-9]([-_.]?[a-zA-Z0-9])*@[a-zA-Z0-9]([-_.]?[a-zA-Z0-9])*\.[a-zA-Z]{2,3}$'
             password_regex = '''^.*(?=.*[a-zA-Z])(?=.*\d)(?=.*[!"#$%&'()*+,\-./:;<=>?@[＼\]^_`{|}~\\)])[\w!"#$%&'()*+,\-./:;<=>?@[＼\]^`{|}~\\)]{8,45}$'''
-            contact_regex = '^\d{11}$'
+            contact_regex  = '^\d{11}$'
+            mbti_regex     = '^[ie][ns][tf][jp]$'
+            gender_list = ['Male', 'Female', 'Undefined']
 
-            if re.match(email_regex, email) is None or re.match(password_regex, password) is None or re.match(contact_regex, contact) is None:
+            if re.match(email_regex, email) is None:
                 raise ValueError
+            if re.match(password_regex, password) is None:
+                raise ValueError
+            if re.match(contact_regex, contact) is None:
+                raise ValueError
+            if mbti and not re.match(mbti_regex, mbti):
+                mbti = None
+            if gender not in gender_list:
+                gender = 'Undefined'
 
-            user          = User()
-            user.name     = name
-            user.email    = email
-            user.password = password
-            user.contact  = contact
-
-            if 'mbti' in data:
-                mbti = data['mbti']
-                if re.match('[ie][ns][tf][jp]', mbti):
-                    user.mbti = mbti
-                else:
-                    raise ValueError
-
-            if 'gender' in data:
-                gender = data['gender']
-                if gender in ['F', 'M', 'U']:
-                    user.gender = gender
-                else:
-                    raise ValueError
+            user = User(
+                name     = name,
+                email    = email,
+                password = password,
+                contact  = contact,
+                mbti     = mbti,
+                gender   = gender,
+            )
 
             user.full_clean()
             user.save()
 
         except:
-            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+            return JsonResponse(
+                {'message': 'KEY_ERROR'},
+                status=400
+            )
 
         return JsonResponse(
-            {'message': 'success - created user('+str(user.id)+')'},
+            {'message': 'CREATED'},
             status=201
         )
