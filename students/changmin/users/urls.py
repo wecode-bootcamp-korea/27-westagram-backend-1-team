feda314fbf8bca238cea44fdfc5c43bf324c894e
Ted0527 <frosthowling@gmail.com>
2021-11-21 16:44:27 +0900

    [제갈창민] fixed some lines following the convention

--- students/changmin/users/urls.py
+++ students/changmin/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from users.views import UserView
+from users.views import UserView, SignInView
 
 urlpatterns = [
         path('', UserView.as_view()),
+        # path('/signin', SignInView.as_view())
         ]
 
--- students/changmin/users/views.py
+++ students/changmin/users/views.py
@@ -5,23 +5,24 @@ from django.http      import JsonResponse
 from users.models     import User
 
 class UserView(View):
-    def post(self, request):
+     def post(self, request):
         data           = json.loads(request.body)
         regex_email    = '[a-zA-Z0-9]+\.?\w*@\w+[.]?\w*[.]+\w{2,3}'
         regex_password = '(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$^&*()+|=])[A-Za-z\d~!@#$%^&*()+|=]{8,}'
 
         try:
             if data['email'] == '' or data['password'] == '':
-                return JsonResponse({'message':'Expected any words in Box'}, status = 400)
-            elif re.match(regex_email, data['email']) is None or re.match(regex_password, data['password']) is None:
                 raise ValueError
-        except ValueError:
-            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
-        else:
-            User.objects.create(
+            elif re.match(regex_email, data['email']) is None or re.match(regex_password, data['password']) is None:
+                return JsonResponse({'message':'Email or Password is not correct'}, status = 400)
+            user_create = User.objects.create(
                 name        = data['name'],
                 email       = data['email'],
                 password    = data['password'],
                 phone       = data['phone']
             )
+            User.full_clean(user_create)
+            User.save(user_create)
             return JsonResponse({'message':'SUCESS'},status=201)
+        except ValueError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
