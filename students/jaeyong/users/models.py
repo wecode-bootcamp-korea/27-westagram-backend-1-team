a899e874c6f3edc186efd702f04bded5b82cfa6a
yajkrap <93360693+yajkrap@users.noreply.github.com>
2021-11-21 18:07:10 +0900

    Update models.py

--- students/jaeyong/users/models.py
+++ students/jaeyong/users/models.py
@@ -4,8 +4,8 @@ class User(models.Model):
     name       = models.CharField(max_length=30)
     email      = models.CharField(max_length=255, unique=True)
     password   = models.CharField(max_length=200)
-    phone      = models.CharField(max_length=35)
-    bioprofile = models.CharField(max_length=150)
+    contact    = models.CharField(max_length=35)
+    profile    = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
