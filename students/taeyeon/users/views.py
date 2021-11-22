6508d5389d43880137f8e7f6cee4d9abb8463650
이태연 <itaeyeon@itaeyeon-ui-MacBookAir.local>
2021-11-22 17:27:03 +0900

    update signup view_3

--- students/taeyeon/users/views.py
+++ students/taeyeon/users/views.py
@@ -7,18 +7,22 @@ from django.core.exceptions import ValidationError
 from .models                import User
 
 class UsersView(View) :
-    def post(self,request) :
+    REGEX_EMAIL    = r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    REGEX_PASSWORD = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+    
+    def post(self, request):
+        
         try: 
             data           = json.loads(request.body)
             email          = data['email']
             password       = data['password']
-            regex_email    = r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-            regex_password = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
-
-            if re.match(regex_email, email) == None :
+            
+            if not re.match(self.REGEX_EMAIL, email) :
                 raise ValidationError("Email format is incorrect")
-            if re.match(regex_password, password) == None :
+            
+            if not re.match(self.REGEX_PASSWORD, password) :
                 raise ValidationError("Password format is incorrect")
+            
             if User.objects.filter(email=email).exists():
                 raise ValidationError("Email already exists")
 
@@ -28,11 +32,13 @@ class UsersView(View) :
                 password = data['password'],
                 phone    = data['phone'],
                 mbti     = data.get('mbti', '')
-                )        
+                )    
+            
+            return JsonResponse({"message" : "SUCCESS"}, status = 201)        
         
         except ValidationError as e : 
             return JsonResponse({"message" : e.message }, status=400)
         except KeyError :
             return JsonResponse({"message" : "KEY_ERROR"}, status=400)
 
-        return JsonResponse({"message" : "SUCCESS"}, status = 201)
+        
