e1c5b595572bd6bb37c96bd874f453187a5b69be
Jaeyong Park <jaejayjaejaypark@gmail.com>
2021-11-22 22:23:34 +0900

    sign up first submit

--- students/jaeyong/users/views.py
+++ students/jaeyong/users/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+import json, re
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+from users.models import User
+
+class SignupView(View) :
+    def post(self,request) :
+
+        data = json.loads(request.body)
+    
+        try: 
+            name            = data['name']
+            email           = data['email']
+            password        = data['password']
+            contact         = data['contact']
+            profile         = data['profile']
+                                
+            regex_email     = r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            regex_password  = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+
+            if not re.match(regex_email, email):
+                return JsonResponse({"message": "Key-Error"}, status=400)
+            if not re.match(regex_password, password):
+                return JsonResponse({"message": "Key-Error"}, status=400)
+            
+
+            User.objects.create(
+                name     = data['name'],
+                email    = data['email'],
+                password = data['password'],
+                contact  = data['contact'],
+                profile  = data['profile'],
+                )
+            
+        except KeyError : 
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)   
+        
+        return JsonResponse({"message" : "SUCCESS"}, status = 201)
