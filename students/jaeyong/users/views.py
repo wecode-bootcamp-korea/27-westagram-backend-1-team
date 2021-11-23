9fae1675ff9b330a98775955605df4cd3b77f70b
Jaeyong Park <jaejayjaejaypark@gmail.com>
2021-11-23 18:00:14 +0900

    SignIn_수정

--- students/jaeyong/users/views.py
+++ students/jaeyong/users/views.py
@@ -52,7 +52,7 @@ class SignInView(View):
             email        = data['email']
             password     = data['password']
 
-            if email == "" or password == "":
+            if email == "" or password =="":
                 return JsonResponse({"message": "Email or Password field is blanked."}, status=401)
             if not User.objects.filter(email=email, password=password).exists():
                 return JsonResponse({"message": "INVALID_USER"}, status=401)
