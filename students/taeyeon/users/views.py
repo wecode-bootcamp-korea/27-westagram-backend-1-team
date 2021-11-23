a6bfc41e670afceb9e39e3209639390dae96d3d0
이태연 <itaeyeon@itaeyeon-ui-MacBookAir.local>
2021-11-23 20:57:07 +0900

    update views.py

--- students/taeyeon/users/views.py
+++ students/taeyeon/users/views.py
@@ -10,6 +10,7 @@ from .models                import User
 class SignUpView(View) :
     
     def post(self, request):
+        
         try: 
             data           = json.loads(request.body)
             email          = data['email']
@@ -30,20 +31,24 @@ class SignUpView(View) :
         
         except ValidationError as e : 
             return JsonResponse({"message" : e.message }, status=400)
+        
         except KeyError :
             return JsonResponse({"message" : "KEY_ERROR"}, status=400)
 
 class SignInView(View) :
    def post(self, request) :
+        
         try :
             data     = json.loads(request.body)
             email    = data['email']
             password = data['password']
+            
             if not User.objects.filter(email=email, password=password).exists() :
                 return JsonResponse({"message" : "INVALID_USER"}, status=401)
             return JsonResponse({"message" : "SUCCESS"}, status=200)
         
         except User.DoesNotExist :
             return JsonResponse({"message" : "INVALID_USER"}, status=401)
+        
         except KeyError :
             return JsonResponse({"message" : "KEY_ERROR"}, status=400)
