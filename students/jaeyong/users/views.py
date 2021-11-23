5b8a9c57047b2580cd7d2056cb60ba497db75213
Jaeyong Park <jaejayjaejaypark@gmail.com>
2021-11-23 13:47:10 +0900

    SignUp 수정1

--- students/jaeyong/users/views.py
+++ students/jaeyong/users/views.py
@@ -2,7 +2,8 @@ import json, re
 
 from django.http  import JsonResponse
 from django.views import View
-from users.models import User
+from .models import User
+from django.core.exceptions import ValidationError
 
 class SignupView(View) :
     def post(self,request) :
@@ -18,13 +19,16 @@ class SignupView(View) :
                                 
             regex_email     = r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
             regex_password  = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
-
-            if not re.match(regex_email, email):
-                return JsonResponse({"message": "Key-Error"}, status=400)
-            if not re.match(regex_password, password):
-                return JsonResponse({"message": "Key-Error"}, status=400)
             
-
+            if User.objects.filter(email=email).exists():
+                raise ValidationError("This email already used.")                    
+            
+            if not re.match(regex_email, email):
+                return JsonResponse({"message": "Email Validation Error"}, status=400)
+            
+            if not re.match(regex_password, password):
+                return JsonResponse({"message": "Password Validation Error"}, status=400)
+            
             User.objects.create(
                 name     = data['name'],
                 email    = data['email'],
@@ -37,3 +41,5 @@ class SignupView(View) :
             return JsonResponse({"message" : "KEY_ERROR"}, status=400)   
         
         return JsonResponse({"message" : "SUCCESS"}, status = 201)
+
+        
